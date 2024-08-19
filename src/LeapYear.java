dd592a453437bf33639bbcee13e38da1c6e37282
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-08-19 13:41:05 -0400

    n

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class LeapYear {
+public class LeapYear {
+    public static void main(String[] args) {
+        System.out.println((-1600));
+        System.out.println((1600));
+        System.out.println((2017));
+        System.out.println((2000));
+    }
+
+    private static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
 }
